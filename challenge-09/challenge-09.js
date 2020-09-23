50a72010fc8c05c011ade931fcc4644e7190d8a9
Miqueias Penha <miqueiaspenha@gmail.com>
2020-09-23 10:18:44 -0300

    challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -12,11 +12,11 @@ indentação correta do código, para ficar dentro da IIFE.
   correto da variável ou função chamada.
   */
   function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log('Na função `myFunction`, o primeiro número é', number1);
     console.log('Na função `myFunction`, o segundo número é', number2);
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
   }
   myFunction();
 
@@ -24,12 +24,12 @@ indentação correta do código, para ficar dentro da IIFE.
       myFunction2();
   */
   function myFunction2() {
-    console.log('A soma de 10 e 20 é igual a', sum ? sum() : undefined);
+    var number1 = 10;
+    var number2 = 20;
     var sum = function sum() {
       return number1 + number2;
     };
-    var number1 = 10;
-    var number2 = 20;
+    console.log('A soma de 10 e 20 é igual a', sum ? sum() : undefined);
     return sum();
   }
   myFunction2();
@@ -38,15 +38,14 @@ indentação correta do código, para ficar dentro da IIFE.
       myFunction3();
   */
   function myFunction3() {
-    console.log('A soma de 40 e 50 é igual a', sum());
-    var number2 = 50;
-    console.log('Na função myFunction3, number1 é igual a', number1);
-    var number1 = 40;
-    return sum();
-
     function sum() {
       return number1 + number2;
     };
+    var number1 = 40;
+    var number2 = 50;
+    console.log('A soma de 40 e 50 é igual a', sum());
+    console.log('Na função myFunction3, number1 é igual a', number1);
+    return sum();
   }
   myFunction3();
 
@@ -63,13 +62,17 @@ indentação correta do código, para ficar dentro da IIFE.
   por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
   que foram passadas para a primeira função `calculator`.
   */
-  // ?
+  function calculator(num1, num2) {
+    return function (callback) {
+      return callback(num1, num2);
+    };
+  }
 
   /*
   Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
   passando dois números por parâmetro.
   */
-  // ?
+  var sum = calculator(3, 4);
 
   /*
   Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -81,14 +84,19 @@ indentação correta do código, para ficar dentro da IIFE.
   anônima tem como seus argumentos.
   */
   console.log('O resultado da soma é:');
-  // ?
+  console.log(sum(function (num1, num2) {
+    return num1 + num2
+  }));
 
   /*
   Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
   `division` e `mod`, e atribua à elas `calculator`, passando números
   diferentes para cada chamada.
   */
-  // ?
+  var subtraction = calculator(7, 5);
+  var multiplication = calculator(7, 5);
+  var division = calculator(8, 2);
+  var mod = calculator(7, 4);
 
   /*
   Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -98,15 +106,23 @@ indentação correta do código, para ficar dentro da IIFE.
   chamada.
   */
   console.log('O resultado da subtração é:');
-  // ?
+  console.log(subtraction(function (num1, num2) {
+    return num1 - num2;
+  }));
 
   console.log('O resultado da multiplicação é:');
-  // ?
+  console.log(multiplication(function (num1, num2) {
+    return num1 * num2;
+  }));
 
   console.log('O resultado da divisão é:');
-  // ?
+  console.log(division(function (num1, num2) {
+    return num1 / num2;
+  }));
 
   console.log('O resto da divisão é:');
-  // ?
+  console.log(mod(function (num1, num2) {
+    return num1 % num2;
+  }));
 
 })();
